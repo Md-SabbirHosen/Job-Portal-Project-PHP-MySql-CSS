e1edbd9ee58a8a3e4fdfba6080f6b5cf71f1dd35
Jawwad926 <mdsabbirhosen926@gmail.com>
2023-06-28 21:01:24 +0600

    commited

--- dashboard/addJob.php
+++ dashboard/addJob.php
@@ -19,8 +19,42 @@
           </div>
           <div class="input-group item-b">
             <label for="job-title">Job Category</label>
-            <input type="text" name="industry" required>
+            <div class="select-container">
+              <select id="select-category">
+                <option value="Accounting/Finance">Accounting/Finance</option>
+                <option value="Bank/Non-Bank Financial Institution">Bank/Non-Bank Financial Institution</option>
+                <option value="Supply Chain/Procurement">Supply Chain/Procurement</option>
+                <option value="Education/Training">Education/Training</option>
+                <option value="Engineer/Architects">Engineer/Architects</option>
+                <option value="Garments/Textile">Garments/Textile</option>
+                <option value="HR/Org. Development">HR/Org. Development</option>
+                <option value="Gen Mgt/Admin">Gen Mgt/Admin</option>
+                <option value="Design/Creative">Design/Creative</option>
+                <option value="Production/Operation">Production/Operation</option>
+                <option value="Hospitality/Travel/Tourism">Hospitality/Travel/Tourism</option>
+                <option value="Commercial">Commercial</option>
+                <option value="Beauty Care/Health & Fitness">Beauty Care/Health & Fitness</option>
+                <option value="IT & Telecommunication">IT & Telecommunication</option>
+                <option value="Marketing/Sales">Marketing/Sales</option>
+                <option value="Customer Service/Call Centre">Customer Service/Call Centre</option>
+                <option value="Media/Ad./Event Mgt.">Media/Ad./Event Mgt.</option>
+                <option value="Medical/Pharma">Medical/Pharma</option>
+                <option value="Agro (Plant/Animal/Fisheries)">Agro (Plant/Animal/Fisheries)</option>
+                <option value="NGO/Development">NGO/Development</option>
+                <option value="Research/Consultancy">Research/Consultancy</option>
+                <option value="Secretary/Receptionist">Secretary/Receptionist</option>
+                <option value="Data Entry/Operator/BPO">Data Entry/Operator/BPO</option>
+                <option value="Driving/Motor Technician">Driving/Motor Technician</option>
+                <option value="Security/Support Service">Security/Support Service</option>
+                <option value="Law/Legal">Law/Legal</option>
+                <option value="Electrician/Construction/Repair">Electrician/Construction/Repair</option>
+              </select>
+              <span class="custom-arrow"></span>
+            </div>
           </div>
+
+
+          <!-- <input type="text" name="industry" required> -->
           <div class="input-group  item-c">
             <label for="job-title">Job Type</label>
             <input type="text" name="job_type" required>
@@ -71,4 +105,6 @@
 
     </div>
   </div>
+
+
 </body>
