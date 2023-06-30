94943d7989852d2f622138939cdcc26fc06856ed
Jawwad926 <mdsabbirhosen926@gmail.com>
2023-06-30 21:54:47 +0600

    commited

--- dashboard/addJob.php
+++ dashboard/addJob.php
@@ -12,7 +12,7 @@
         <div class="headline">
           <h3>Add New Job</h3>
         </div>
-        <form class="job-form-container">
+        <form class="job-form-container" method="post" action="../process/newJob.php">
           <div class="input-group item-a">
             <label for="job-title">Job Title/Designation</label>
             <input type="text" placeholder="Job Title" name="jobtitle" required>
@@ -20,34 +20,13 @@
           <div class="input-group item-b">
             <label for="job-title">Job Category</label>
             <div class="select-container">
-              <select id="select-category">
-                <option value="Accounting/Finance">Accounting/Finance</option>
-                <option value="Bank/Non-Bank Financial Institution">Bank/Non-Bank Financial Institution</option>
-                <option value="Supply Chain/Procurement">Supply Chain/Procurement</option>
-                <option value="Education/Training">Education/Training</option>
-                <option value="Engineer/Architects">Engineer/Architects</option>
-                <option value="Garments/Textile">Garments/Textile</option>
-                <option value="HR/Org. Development">HR/Org. Development</option>
-                <option value="Gen Mgt/Admin">Gen Mgt/Admin</option>
-                <option value="Design/Creative">Design/Creative</option>
-                <option value="Production/Operation">Production/Operation</option>
-                <option value="Hospitality/Travel/Tourism">Hospitality/Travel/Tourism</option>
-                <option value="Commercial">Commercial</option>
-                <option value="Beauty Care/Health & Fitness">Beauty Care/Health & Fitness</option>
-                <option value="IT & Telecommunication">IT & Telecommunication</option>
-                <option value="Marketing/Sales">Marketing/Sales</option>
-                <option value="Customer Service/Call Centre">Customer Service/Call Centre</option>
-                <option value="Media/Ad./Event Mgt.">Media/Ad./Event Mgt.</option>
-                <option value="Medical/Pharma">Medical/Pharma</option>
-                <option value="Agro (Plant/Animal/Fisheries)">Agro (Plant/Animal/Fisheries)</option>
-                <option value="NGO/Development">NGO/Development</option>
-                <option value="Research/Consultancy">Research/Consultancy</option>
-                <option value="Secretary/Receptionist">Secretary/Receptionist</option>
-                <option value="Data Entry/Operator/BPO">Data Entry/Operator/BPO</option>
-                <option value="Driving/Motor Technician">Driving/Motor Technician</option>
-                <option value="Security/Support Service">Security/Support Service</option>
-                <option value="Law/Legal">Law/Legal</option>
-                <option value="Electrician/Construction/Repair">Electrician/Construction/Repair</option>
+              <select id="select-category" name="industry">
+                <?php $jobCategorySql = "SELECT * from industry";
+                $jobCategoryQuery = $conn->query($jobCategorySql);
+                while ($jobCategory = $jobCategoryQuery->fetch_assoc()) {
+                ?>
+                  <option value="<?php echo $jobCategory['id'] ?>"><?php echo $jobCategory['name'] ?></option>
+                <?php } ?>
               </select>
               <span class="custom-arrow"></span>
             </div>
@@ -55,10 +34,13 @@
           <div class="input-group  item-c">
             <label for="job-title">Job Type</label>
             <div class="select-container">
-              <select id="select-category">
-                <option value="Full Time">Full Time</option>
-                <option value="Part Time">Part Time</option>
-                <option value="Internship">Internship</option>
+              <select id="select-category" name="job_type">
+                <?php $jobTypeSql = "SELECT * from job_type";
+                $jobTypeQuery = $conn->query($jobTypeSql);
+                while ($jobType = $jobTypeQuery->fetch_assoc()) {
+                ?>
+                  <option value="<?php echo $jobType['id'] ?>"><?php echo $jobType['type'] ?></option>
+                <?php } ?>
               </select>
               <span class="custom-arrow"></span>
             </div>
@@ -69,17 +51,27 @@
           </div>
           <div class="input-group  item-e">
             <label for="job-title">Edu. Qualification</label>
-            <input type="text" name="edu_qualification" required>
+            <div class="select-container">
+              <select id="select-category" name="edu_qualification">
+                <?php $educationSql = "SELECT * from education";
+                $educationQuery = $conn->query($educationSql);
+                while ($education = $educationQuery->fetch_assoc()) {
+                ?>
+                  <option value="<?php echo $education['id'] ?>"><?php echo $education['name'] ?></option>
+                <?php } ?>
+              </select>
+              <span class="custom-arrow"></span>
+            </div>
           </div>
           <div class="input-group  item-f">
             <label for="job-title">Division/State</label>
             <div class="select-container">
-              <select id="select-category" name="region" required>
-                <?php $divisionSql = "SELECT * from states";
-                $divisionQuery = $conn->query($divisionSql);
-                while ($division = $divisionQuery->fetch_assoc()) {
+              <select id="select-category" name="division_or_state" required>
+                <?php $divisionOrStateSql = "SELECT * from states";
+                $divisionOrStateQuery = $conn->query($divisionOrStateSql);
+                while ($divisionOrState = $divisionOrStateQuery->fetch_assoc()) {
                 ?>
-                  <option value="<?php echo $division['id'] ?>"><?php echo $division['name'] ?></option>
+                  <option value="<?php echo $divisionOrState['id'] ?>"><?php echo $divisionOrState['name'] ?></option>
                 <?php } ?>
               </select>
               <span class="custom-arrow"></span>
@@ -88,70 +80,13 @@
           <div class="input-group  item-g">
             <label for="job-title">District/City</label>
             <div class="select-container">
-              <select id="select-category">
-                <option value="Dhaka">Dhaka</option>
-                <option value="Chittagong">Chittagong</option>
-                <option value="Sylhet">Sylhet</option>
-                <option value="Rajshahi">Rajshahi</option>
-                <option value="Khulna">Khulna</option>
-                <option value="Comilla">Comilla</option>
-                <option value="Barisal">Barisal</option>
-                <option value="Mymensingh">Mymensingh</option>
-                <option value="Cox's Bazar">Cox's Bazar</option>
-                <option value="Rangpur">Rangpur</option>
-                <option value="Jessore">Jessore</option>
-                <option value="Narayanganj">Narayanganj</option>
-                <option value="Gazipur">Gazipur</option>
-                <option value="Bogra">Bogra</option>
-                <option value="Dinajpur">Dinajpur</option>
-                <option value="Tangail">Tangail</option>
-                <option value="Jamalpur">Jamalpur</option>
-                <option value="Pabna">Pabna</option>
-                <option value="Narsingdi">Narsingdi</option>
-                <option value="Naogaon">Naogaon</option>
-                <option value="Kushtia">Kushtia</option>
-                <option value="Rangamati">Rangamati</option>
-                <option value="Cox's Bazar">Cox's Bazar</option>
-                <option value="Noakhali">Noakhali</option>
-                <option value="Bhola">Bhola</option>
-                <option value="Sherpur">Sherpur</option>
-                <option value="Kishoreganj">Kishoreganj</option>
-                <option value="Satkhira">Satkhira</option>
-                <option value="Chandpur">Chandpur</option>
-                <option value="Patuakhali">Patuakhali</option>
-                <option value="Bagerhat">Bagerhat</option>
-                <option value="Sirajganj">Sirajganj</option>
-                <option value="Moulvibazar">Moulvibazar</option>
-                <option value="Faridpur">Faridpur</option>
-                <option value="Brahmanbaria">Brahmanbaria</option>
-                <option value="Kushtia">Kushtia</option>
-                <option value="Panchagarh">Panchagarh</option>
-                <option value="Sunamganj">Sunamganj</option>
-                <option value="Narail">Narail</option>
-                <option value="Chuadanga">Chuadanga</option>
-                <option value="Joypurhat">Joypurhat</option>
-                <option value="Netrokona">Netrokona</option>
-                <option value="Magura">Magura</option>
-                <option value="Thakurgaon">Thakurgaon</option>
-                <option value="Lakshmipur">Lakshmipur</option>
-                <option value="Meherpur">Meherpur</option>
-                <option value="Jhenaidah">Jhenaidah</option>
-                <option value="Lalmonirhat">Lalmonirhat</option>
-                <option value="Gaibandha">Gaibandha</option>
-                <option value="Satkhira">Satkhira</option>
-                <option value="Habiganj">Habiganj</option>
-                <option value="Barguna">Barguna</option>
-                <option value="Shariatpur">Shariatpur</option>
-                <option value="Chapai Nawabganj">Chapai Nawabganj</option>
-                <option value="Khagrachhari">Khagrachhari</option>
-                <option value="Manikganj">Manikganj</option>
-                <option value="Nilphamari">Nilphamari</option>
-                <option value="Jhalokati">Jhalokati</option>
-                <option value="Pirojpur">Pirojpur</option>
-                <option value="Madaripur">Madaripur</option>
-                <option value="Bandarban">Bandarban</option>
-                <option value="Natore">Natore</option>
-                <option value="Munshiganj">Munshiganj</option>
+              <select id="select-category" name="district_or_city">
+                <?php $districtOrCitySql = "SELECT * from districts_or_cities";
+                $districtOrCityQuery = $conn->query($districtOrCitySql);
+                while ($districtOrCity = $districtOrCityQuery->fetch_assoc()) {
+                ?>
+                  <option value="<?php echo $districtOrCity['id'] ?>"><?php echo $districtOrCity['name'] ?></option>
+                <?php } ?>
               </select>
               <span class="custom-arrow"></span>
             </div>
@@ -176,10 +111,13 @@
             <label for="job-title">Job Description</label>
             <textarea cols="20" rows="5" placeholder="Job Description..." required="" name="description"></textarea>
           </div>
-          <div class="button-container item-m">
-            <button type="submit" name="myProfile" class="btn btn-secondary">Submit Job</button>
+          <div class="input-group  item-m">
+            <label for="job-title">Responsibilities</label>
+            <textarea cols="20" rows="5" placeholder="Responsibilites..." required="" name="responsibility"></textarea>
           </div>
-
+          <div class="button-container item-n">
+            <button type="submit" name="submitJob" class="btn btn-secondary">Submit Job</button>
+          </div>
         </form>
 
       </div>
---
+++ process/newJob.php
@@ -0,0 +1,29 @@
+<?php
+include "../includes/session.php";
+
+if (isset($_POST['submitJob'])) {
+  $id_company = $_SESSION['id_company'];
+  $jobtitle = $_POST['jobtitle'];
+  $industry = $_POST['industry'];
+  $job_type = $_POST['job_type'];
+  $experience = $_POST['experience'];
+  $edu_qualification = $_POST['edu_qualification'];
+  $division_or_state_id = $_POST['division_or_state'];
+  $district_or_city_id = $_POST['district_or_city'];
+  $minimumsalary = $_POST['minimumsalary'];
+  $maximumsalary = $_POST['maximumsalary'];
+  $deadline = $_POST['deadline'];
+  $skills_ability = $_POST['skills'];
+  $description = $_POST['description'];
+  $responsibility = $_POST['responsibility'];
+
+
+  $sql = "INSERT INTO job_post (id_company, jobtitle, industry_id, job_status, description, minimumsalary, maximumsalary, state_id, city_id, experience, skills_ability, edu_qualification, responsibility, deadline) VALUES ('$id_company', '$jobtitle', '$industry', '$job_type', '$description', '$minimumsalary', '$maximumsalary', '$division_or_state_id', '$district_or_city_id', '$experience', '$skills_ability', '$edu_qualification', '$responsibility', '$deadline')";
+
+  if ($conn->query($sql)) {
+    echo "Jobs added successfully!";
+  }
+}
+
+header('Location: ../dashboard/addJob.php');
+exit();
