d6156d35fab02c5f03cbdc5272d27cbf43b6e8a0
Jawwad926 <mdsabbirhosen926@gmail.com>
2023-07-01 17:51:47 +0600

    commited

--- dashboard/manageJobs.php
+++ dashboard/manageJobs.php
@@ -10,40 +10,65 @@
       <div class="headline">
         <h3>My Jobs Listings</h3>
       </div>
-      <div class="job-item-container">
-        <div class="profile-container">
-          <img src="../assets/images/9bbb5f8aa2c4d5ac9ef31dec610ba406shreemangal.jpg" alt="">
-        </div>
-        <div class="job-info-container">
-          <p>Active</p>
-          <div class="title-with-job-status">
-            <h3>Java Developer (Software Engineer)</h3>
-            <div class="job-status">
-              <i class="fa-solid fa-briefcase"></i>
-              <p>Full Time</p>
-            </div>
-          </div>
-          <div class="others-info">
-            <div class="job-category-info">
-              <i class="fa-solid fa-briefcase"></i>
-              <p>Telecommunications industry</p>
-            </div>
-            <div class="date-info">
-              <i class="fa-solid fa-calendar-days"></i>
-              <p>2023-6-28 17:01:56</p>
-            </div>
-            <div class="salary-info">
-              <i class="fa-solid fa-money-check-dollar"></i>
-              <p>$2500-$3500</p>
-            </div>
-            <div class="location-info">
-              <i class="fa-solid fa-location-dot"></i>
-              <p>Dhaka&Chittagong</p>
-            </div>
-          </div>
+      <?php
+      $id_company  = $_SESSION['id_company'];
 
+      $sql = "SELECT * from job_post where id_company = '$id_company' order by id_jobpost desc";
+      $result = $conn->query($sql);
+
+      while ($row = $result->fetch_assoc()) {
+        $jobtitle = $row['jobtitle'];
+        $city_id = $row['city_id'];
+        $industry_id = $row['industry_id'];
+        $job_status_id = $row['job_status'];
+        $minimum_salary = $row['minimumsalary'];
+        $maximum_salary = $row['maximumsalary'];
+        $create_date = $row['createdat'];
+        $location = $conn->query("SELECT name from districts_or_cities where id = '$city_id'");
+        $job_category = $conn->query("SELECT name from industry where id = '$industry_id'");
+        $job_type = $conn->query("SELECT type from job_type where id = '$job_status_id'");
+        $profile_pic = $conn->query("SELECT profile_pic from company where id_company = '$id_company'");
+
+        $location = $location->fetch_assoc();
+        $job_category = $job_category->fetch_assoc();
+        $job_type = $job_type->fetch_assoc();
+        $profile_pic = $profile_pic->fetch_assoc();
+
+      ?>
+        <div class="job-item-container">
+          <div class="profile-container">
+            <img src="../assets/images/<?php echo $profile_pic['profile_pic'] ?>" alt="">
+          </div>
+          <div class="job-info-container">
+            <span class="validity-info">Active</span>
+            <div class="title-with-job-status">
+              <h3> <?php echo $jobtitle; ?> </h3>
+              <div class="job-status">
+                <i class="fa-solid fa-briefcase"></i>
+                <span><?php echo $job_type['type'] ?></span>
+              </div>
+            </div>
+            <div class="others-info">
+              <div class="job-category-info">
+                <i class="fa-solid fa-briefcase"></i>
+                <span><?php echo $job_category['name'] ?></span>
+              </div>
+              <div class="date-info">
+                <i class="fa-solid fa-calendar-days"></i>
+                <span><?php echo $create_date; ?></span>
+              </div>
+              <div class="salary-info">
+                <i class="fa-solid fa-money-check-dollar"></i>
+                <span><?php echo $minimum_salary . "-" . $maximum_salary ?></span>
+              </div>
+              <div class="location-info">
+                <i class="fa-solid fa-location-dot"></i>
+                <span><?php echo $location['name'] ?></span>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      <?php } ?>
     </div>
   </div>
 </body>
