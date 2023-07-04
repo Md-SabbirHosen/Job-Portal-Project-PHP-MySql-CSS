5ed4b10a3df02cf0531edfd8261af5b91ff70f01
Jawwad926 <mdsabbirhosen926@gmail.com>
2023-07-05 01:58:07 +0600

    commited

--- findJobs.php
+++ findJobs.php
@@ -57,37 +57,46 @@
           $profile_pic = $profile_pic->fetch_assoc();
 
         ?>
-
           <div class="job-item-container">
             <div class="profile-container">
               <img src="../assets/images/<?php echo $profile_pic['profile_pic'] ?>" alt="">
             </div>
             <div class="job-info-container">
-              <span class="validity-info">Active</span>
-              <div class="title-with-job-status">
-                <h3> <?php echo $jobtitle; ?> </h3>
+              <div class="job-info-left-side">
                 <div class="job-status">
                   <i class="fa-solid fa-briefcase"></i>
                   <span><?php echo $job_type['type'] ?></span>
                 </div>
-              </div>
-              <div class="others-info">
-                <div class="job-category-info">
-                  <i class="fa-solid fa-briefcase"></i>
-                  <span><?php echo $job_category['name'] ?></span>
+                <h3> <?php echo $jobtitle; ?> </h3>
+                <div class="others-info">
+                  <div class="job-category-info">
+                    <i class="fa-solid fa-briefcase"></i>
+                    <span><?php echo $job_category['name'] ?></span>
+                  </div>
+                  <div class="salary-info">
+                    <i class="fa-solid fa-money-check-dollar"></i>
+                    <span><?php echo $minimum_salary . "-" . $maximum_salary ?></span>
+                  </div>
+                  <div class="location-info">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <span><?php echo $location['name'] ?></span>
+                  </div>
                 </div>
                 <div class="date-info">
                   <i class="fa-solid fa-calendar-days"></i>
                   <span><?php echo $create_date; ?></span>
                 </div>
-                <div class="salary-info">
-                  <i class="fa-solid fa-money-check-dollar"></i>
-                  <span><?php echo $minimum_salary . "-" . $maximum_salary ?></span>
-                </div>
-                <div class="location-info">
-                  <i class="fa-solid fa-location-dot"></i>
-                  <span><?php echo $location['name'] ?></span>
-                </div>
+              </div>
+              <div class="job-info-right-side">
+                <?php $deadline = date_create($row['deadline']);
+                $now = date_create(date("y-m-d"));
+                if ($now < $deadline) {
+                  echo "<span class=" . "validity-active" . ">Active</span>";
+                } else {
+                  echo "<span class=" . "validity-expired" . ">Expired</span>";
+                }
+                ?>
+
               </div>
             </div>
           </div>
