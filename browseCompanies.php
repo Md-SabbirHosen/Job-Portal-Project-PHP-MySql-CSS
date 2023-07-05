41abda81e3f904c56cce4c2100fd13cd76d7ad72
Jawwad926 <mdsabbirhosen926@gmail.com>
2023-07-06 03:58:25 +0600

    commited

--- browseCompanies.php
+++ browseCompanies.php
@@ -16,5 +16,46 @@
         </div>
       </div>
     </div>
+    <section class="page-content">
+      <div class="page-content-left-side">
+        <?php include "./includes/searchSidebar.php" ?>
+      </div>
+
+      <div class="page-content-right-side">
+        <?php
+        $sql = "SELECT * from company";
+        $query = $conn->query($sql);
+        while ($row = $query->fetch_assoc()) {
+          $industry_id = $row['industry_id'];
+          $state_id = $row['state_id'];
+          $city_id = $row['city_id'];
+
+          $industry = $conn->query("SELECT name from industry where id = '$industry_id'");
+          $division_or_state =
+            $conn->query("SELECT name from states where id = '$state_id'");
+          $district_or_city = $conn->query("SELECT name from districts_or_cities where id = '$city_id'");
+
+          $industry = $industry->fetch_assoc();
+          $division_or_state = $division_or_state->fetch_assoc();
+          $district_or_city = $district_or_city->fetch_assoc();
+        ?>
+          <div class="company-item">
+            <div class="profile-container">
+              <img src="../assets/images/<?php echo $row['profile_pic'] ?>" alt="">
+            </div>
+            <div class="job-info-container">
+              <h3> <?php echo $row['companyname'] ?> </h3>
+              <div class="job-category-info">
+                <i class="fa-solid fa-briefcase"></i>
+                <span><?php echo $industry['name'] ?></span>
+              </div>
+              <div class="location-info">
+                <i class="fa-solid fa-location-dot"></i>
+                <span><?php echo $division_or_state['name'] . ', ' . $district_or_city['name']; ?></span>
+              </div>
+            </div>
+          </div>
+        <?php } ?>
+      </div>
   </div>
 </body>
