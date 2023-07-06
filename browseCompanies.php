d9ad6bfd3fb860cc505b4df0a2e7a5ff78eb7406
Jawwad926 <mdsabbirhosen926@gmail.com>
2023-07-06 17:25:11 +0600

    commited

--- browseCompanies.php
+++ browseCompanies.php
@@ -8,8 +8,9 @@
       <div class="intro-banner-overlay">
         <div class="intro-banner-content">
           <div class="container">
-            <div class="banner-headline-text-part">
+            <div class="banner-headline-text-part glassmorphism">
               <h3>Browse Companies</h3>
+              <div class="line line-dark"></div>
               <span>Discover a diverse range of companies and explore their offerings. Whether you're seeking employment opportunities or looking to learn more about different industries, our curated list of companies has got you covered.</span>
             </div>
           </div>
--- companyDetails.php
+++ companyDetails.php
@@ -6,56 +6,47 @@
   <?php if (isset($_GET['key']) && isset($_GET['id'])) :
     $company_id = $_GET['id'];
 
+    $sql = "SELECT * FROM company WHERE id_company='$company_id'";
+    $query = $conn->query($sql);
+    $row = $query->fetch_assoc();
+
+    //getting state info
+    $division_or_state_id = $row['state_id'];
+    $sql1 =  "SELECT * FROM states WHERE id = '$division_or_state_id'";
+    $query1 = $conn->query($sql1);
+    $row1 = $query1->fetch_assoc();
+
+    //getting city info
+    $district_or_city_id = $row['city_id'];
+    $sql2 =  "SELECT * FROM districts_or_cities WHERE id = '$district_or_city_id'";
+    $query2 = $conn->query($sql2);
+    $row2 = $query2->fetch_assoc();
+
+    //getting industry info
+    $industry_id = $row['industry_id'];
+    $sql3 =  "SELECT * FROM industry WHERE id = '$industry_id'";
+    $query3 = $conn->query($sql3);
+    $row3 = $query3->fetch_assoc();
 
   ?>
-    <div id="browse-company-page">
+    <div id="browse-company-details">
       <div class="intro-banner">
         <div class="intro-banner-overlay">
           <div class="intro-banner-content">
             <div class="container">
-              <div class="banner-headline-text-part">
-                <div class="job-item-container">
-                  <div class="profile-container">
-                    <img src="../assets/images/<?php echo $profile_pic['profile_pic'] ?>" alt="">
+              <div class="banner-headline-text-part glassmorphism">
+                <div class="profile-container">
+                  <img src="../assets/images/<?php echo $row['profile_pic'] ?>" alt="">
+                </div>
+                <div class="job-info-container">
+                  <h3> <?php echo $row['companyname'] ?> </h3>
+                  <div class="job-category-info">
+                    <i class="fa-solid fa-briefcase"></i>
+                    <span><?php echo $row3['name'] . " industry" ?></span>
                   </div>
-                  <div class="job-info-container">
-                    <div class="job-info-left-side">
-                      <div class="job-status">
-                        <i class="fa-solid fa-briefcase"></i>
-                        <span><?php echo $job_type['type'] ?></span>
-                      </div>
-                      <h3> <?php echo $jobtitle; ?> </h3>
-                      <div class="others-info">
-                        <div class="job-category-info">
-                          <i class="fa-solid fa-briefcase"></i>
-                          <span><?php echo $job_category['name'] ?></span>
-                        </div>
-                        <div class="salary-info">
-                          <i class="fa-solid fa-money-check-dollar"></i>
-                          <span><?php echo $minimum_salary . "-" . $maximum_salary ?></span>
-                        </div>
-                        <div class="location-info">
-                          <i class="fa-solid fa-location-dot"></i>
-                          <span><?php echo $location['name'] ?></span>
-                        </div>
-                      </div>
-                      <div class="date-info">
-                        <i class="fa-solid fa-calendar-days"></i>
-                        <span><?php echo $create_date; ?></span>
-                      </div>
-                    </div>
-                    <div class="job-info-right-side">
-                      <?php
-                      $deadline = date_create($row['deadline']);
-                      $now = date_create(date("y-m-d"));
-
-                      if ($now < $deadline) {
-                        echo "<span class=" . "validity-active" . ">Active</span>";
-                      } else {
-                        echo "<span class=" . "validity-expired" . ">Expired</span>";
-                      }
-                      ?>
-                    </div>
+                  <div class="location-info">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <span><?php echo $row1['name'] . ", " . $row2['name'] ?></span>
                   </div>
                 </div>
               </div>
