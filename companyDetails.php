e8eeee00ee106f29a670a9799e3f8630f9ae8ed9
Jawwad926 <mdsabbirhosen926@gmail.com>
2023-08-10 15:46:51 +0600

    commmited

--- companyDetails.php
+++ companyDetails.php
@@ -146,8 +146,42 @@
             <?php }
             ?>
           </div>
+          <div class="review-container-area">
+            <div class="headline">
+              <span class="icon-container">
+                <i class="fa-solid fa-star"></i>
+              </span>
+              <h3>Review Company</h3>
+            </div>
+            <div class="review-container">
+              <div class="post">
+                <div class="text">Thanks for rating us!</div>
+                <div class="edit">EDIT</div>
+              </div>
+              <div class="star-widget">
+                <input type="radio" name="rate" id="rate-5">
+                <label for="rate-5" class="fas fa-star"></label>
+                <input type="radio" name="rate" id="rate-4">
+                <label for="rate-4" class="fas fa-star"></label>
+                <input type="radio" name="rate" id="rate-3">
+                <label for="rate-3" class="fas fa-star"></label>
+                <input type="radio" name="rate" id="rate-2">
+                <label for="rate-2" class="fas fa-star"></label>
+                <input type="radio" name="rate" id="rate-1">
+                <label for="rate-1" class="fas fa-star"></label>
+                <form>
+                  <header></header>
+                  <div class="textarea">
+                    <textarea cols="30" placeholder="Describe your experience.."></textarea>
+                  </div>
+                  <div class="button-container">
+                    <button class="btn" type="submit">Submit Review</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-
         <div class="company-details-page-content-right-side">
           <div class="information-wrapper">
             <div class="information-container">
