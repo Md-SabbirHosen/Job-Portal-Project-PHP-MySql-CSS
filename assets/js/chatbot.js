a3b82833ad6c164cdd8b31d4dd71319551ffd5e7
Jawwad926 <mdsabbirhosen926@gmail.com>
2024-01-30 15:04:28 +0600

    chatbot system added

---
+++ assets/js/chatbot.js
@@ -0,0 +1,90 @@
+const chatbotBtn = document.querySelector(".chatbot-btn");
+const chatbotCloseBtn = document.querySelector(".close-btn");
+const chatArea = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendBtn = document.querySelector(".chat-input span");
+
+let messages = null;
+const API_KEY = "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";
+const inputHeight = chatInput.scrollHeight;
+
+const chatList = (message, className) => {
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", className);
+  let chatContent =
+    className === "outgoing"
+      ? "<p></p>"
+      : '<span class="icon"><i class="fa-regular fa-envelope"></i></span><p></p>';
+  chatLi.innerHTML = chatContent;
+  chatLi.querySelector("p").textContent = message;
+  return chatLi;
+};
+
+const generateResponse = (chatElement) => {
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+  const messageElement = chatElement.querySelector("p");
+
+  const formateOfRequest = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: messages }],
+    }),
+  };
+
+  fetch(API_URL, formateOfRequest)
+    .then((res) => res.json())
+    .then((data) => {
+      messageElement.textContent = data.choices[0].message.content.trim();
+    })
+    .catch(() => {
+      messageElement.classList.add("error");
+      messageElement.textContent =
+        "Hello,user there might be some issues.Please Try Again!";
+    })
+    .finally(() => chatArea.scrollTo(0, chatArea.scrollHeight));
+};
+
+const handleChat = () => {
+  messages = chatInput.value.trim();
+  if (!messages) return;
+
+  chatInput.value = "";
+  chatInput.style.height = `${inputHeight}px`;
+
+  chatArea.appendChild(chatList(messages, "outgoing"));
+  chatArea.scrollTo(0, chatArea.scrollHeight);
+
+  setTimeout(() => {
+    const incomingChatLi = chatList("...", "incoming");
+    chatArea.appendChild(incomingChatLi);
+    chatArea.scrollTo(0, chatArea.scrollHeight);
+    generateResponse(incomingChatLi);
+  }, 100);
+};
+
+chatInput.addEventListener("input", () => {
+  chatInput.style.height = `${inputHeight}px`;
+  chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+sendBtn.addEventListener("click", handleChat);
+
+chatbotBtn.addEventListener("click", () =>
+  document.body.classList.toggle("show-chatbot")
+);
+
+chatbotCloseBtn.addEventListener("click", () =>
+  document.body.classList.remove("show-chatbot")
+);
+
+chatInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+    e.preventDefault();
+    handleChat();
+  }
+});
---
+++ includes/indexChat.php
@@ -0,0 +1,26 @@
+<div class="chatbot">
+  <header>
+    <span class="close-btn">close</span>
+    <div class="header-container">
+      <div class="icon"><i class="fa-regular fa-envelope"></i></div>
+      <div class="heading">
+        <h3>Supporting Bot</h3>
+        <p>online</p>
+      </div>
+    </div>
+  </header>
+  <ul class="chatbox">
+    <li class="chat incoming">
+      <span class="icon"><i class="fa-regular fa-envelope"></i></span>
+      <p>👋 Welcome to Support Bot.I'm Chatbot,your AI Assistant.Let me know how can I help you?</p>
+    </li>
+  </ul>
+  <div class="chat-input">
+    <textarea placeholder="Type a message here..." spellcheck="false" required></textarea>
+    <span id="send-btn" class="send-icon"><i class="fa-solid fa-paper-plane"></i></span>
+  </div>
+</div>
+<button class="chatbot-btn">
+  <span class=""><i class="fa-brands fa-facebook-messenger"></i></span>
+  <span class=""><i class="fa-solid fa-circle-xmark"></i></span>
+</button>
